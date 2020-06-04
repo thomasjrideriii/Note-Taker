e6f086056549404b99e946f24cca92e0c20ada60
Thomas Rider <thomasjrideriii@gmail.com>
2020-06-03 21:26:10 -0400

    Edited server.js to begin buildling api functionality

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const path = require("path")
-const PORT = process.env.PORT || 3000;
+const path = require("path");
+const fs = require("fs");
+const PORT = process.env.PORT || 8080;
+// const crypto = require("crypto");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,11 +13,42 @@ app.use(express.json());
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 });
-
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"))
 });
 
+
+// API Routes
+
+app.get("/api/notes", function(req, res) {
+    console.log("GET!")
+    const notesData = fs.readFile("./db/db.json", utf8, function(err) {
+        console.log(notesData)
+        if (err) {
+            throw err
+        }
+        res.json(notesData)
+    })
+})
+
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    newNote.id = crypto.randomBytes(10).toString("hex");
+
+    console.log(newNote);
+
+    res.json(newNote)
+    fs.appendFile("./db/db.json", JSON.stringify(newNote), function(err) {
+        if (err) {
+            throw err
+        }
+    })
+    
+})
+
+app.delete("/app/notes/:id", function(req, res) {
+    res.end()
+})
 
 app.listen(PORT, function() {
     console.log("Listening on port : " + PORT)
